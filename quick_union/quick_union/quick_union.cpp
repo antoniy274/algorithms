0002d330bfa6f2b6250fe68699b24d4213801e93
Даниил Ярушин <antoniy274@gmail.com>
2022-04-27 19:09:38 +0300

    add code

---
+++ quick_union/quick_union/quick_union.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+static const int N = 10;
+int main()
+{
+	int i, j, p, q, id[N];
+	for (i = 0; i < N; ++i) id[i] = i;
+	while (std::cin >> p >> q) {
+		for (i = p; i != id[i]; i = id[i]);
+		for (j = q; j != id[j]; j = id[j]);
+		if (i == j) continue;
+		id[i] = j;
+		std::cout << p << " " << q << "\n";
+	}
+}
